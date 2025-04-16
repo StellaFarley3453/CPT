36e709667207559867c40c3525afb2b08e328890
StellaFarley3453 <stella.farley@student.csd509j.net>
2025-04-16 16:31:37 -0700

    Update pxt.json, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
- 
+//set things up
+tiles.setCurrentTilemap(tilemap`level`)
+let princess = sprites.create(assets.image`princess_facing_down`)
+scene.cameraFollowSprite(princess)
+controller.moveSprite(princess)
+tiles.placeOnTile(princess, tiles.getTileLocation(53, 7))
