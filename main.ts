45944a60c614ccd6ecd9f4d8871bcf9958ff5fb3
StellaFarley3453 <stella.farley@student.csd509j.net>
2025-04-17 11:27:20 -0700

    Update main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ let princess = sprites.create(assets.image`princess_facing_down`)
 scene.cameraFollowSprite(princess)
 controller.moveSprite(princess)
 tiles.placeOnTile(princess, tiles.getTileLocation(53, 7))
+let interactbox:Sprite = null
 
 // this variable is to keep track of what direction the player sprite is facing.
 // 1 is up, 2 is right, 3 is down, 4 is left.
@@ -12,34 +13,70 @@ let direction = 3
 //event handlers
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     direction = 4
-    animation.runImageAnimation(princess,)
+    animation.runImageAnimation(princess,assets.animation`princess_Walk_Left`,100,true)
 })
-
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     direction = 2
-    princess.setImage(assets.image`myImage`)
+    animation.runImageAnimation(princess, assets.animation`princess_Walk_Right`, 100, true)
 })
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     direction = 1
-    princess.setImage(assets.image`myImage`)
+    animation.runImageAnimation(princess, assets.animation`princess_Walk_Up`, 100, true)
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     direction = 3
-    princess.setImage(assets.image`myImage`)
+    animation.runImageAnimation(princess, assets.animation`princess_Walk_Down`, 100, true)
 })
 controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    if (!controller.up.isPressed() && !controller.left.isPressed() && !controller.right.isPressed() && !controller.down.isPressed()){
     animation.stopAnimation(animation.AnimationTypes.All, princess)
-    princess.setImage(assets.image`princess_facing_left`)
+    }
+    //princess.setImage(assets.image`princess_facing_left`)
 })
 controller.right.onEvent(ControllerButtonEvent.Released, function () {
-    animation.stopAnimation(animation.AnimationTypes.All, princess)
-    princess.setImage(assets.image`princess_facing_right`)
+    if (!controller.up.isPressed() && !controller.left.isPressed() && !controller.right.isPressed() && !controller.down.isPressed()) {
+        animation.stopAnimation(animation.AnimationTypes.All, princess)
+    }
+    //princess.setImage(assets.image`princess_facing_right`)
 })
 controller.up.onEvent(ControllerButtonEvent.Released, function () {
-    animation.stopAnimation(animation.AnimationTypes.All, princess)
-    princess.setImage(assets.image`princess_facing_up`)
+    if (!controller.up.isPressed() && !controller.left.isPressed() && !controller.right.isPressed() && !controller.down.isPressed()) {
+        animation.stopAnimation(animation.AnimationTypes.All, princess)
+    }
+    //princess.setImage(assets.image`princess_facing_up`)
 })
 controller.down.onEvent(ControllerButtonEvent.Released, function () {
-    animation.stopAnimation(animation.AnimationTypes.All, princess)
-    princess.setImage(assets.image`princess_facing_down`)
+    if (!controller.up.isPressed() && !controller.left.isPressed() && !controller.right.isPressed() && !controller.down.isPressed()) {
+        animation.stopAnimation(animation.AnimationTypes.All, princess)
+    }
+    //princess.setImage(assets.image`princess_facing_down`)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed,function(){
+if (direction == 1) {
+    let interactbox = sprites.create(assets.image`myImage`, SpriteKind.Projectile)
+    interactbox.lifespan = 500
+    interactbox.setPosition(princess.x,princess.y-16)
+    //interactbox.setFlag(SpriteFlag.Invisible,true)
+}
+if (direction == 2) {
+    let interactbox = sprites.create(assets.image`myImage`, SpriteKind.Projectile)
+    interactbox.lifespan = 500
+    interactbox.setPosition(princess.x + 16, princess.y)
+    interactbox.setFlag(SpriteFlag.Invisible, true)
+}
+if (direction == 3) {
+    let interactbox = sprites.create(assets.image`myImage`, SpriteKind.Projectile)
+    interactbox.lifespan = 500
+    interactbox.setPosition(princess.x, princess.y + 16)
+    interactbox.setFlag(SpriteFlag.Invisible, true)
+}
+if (direction == 4) {
+    interactbox = sprites.create(assets.image`myImage`, SpriteKind.Projectile)
+    interactbox.lifespan = 500
+    interactbox.setPosition(princess.x - 16, princess.y)
+    interactbox.setFlag(SpriteFlag.Invisible, true)
+}
+})
+scene.onOverlapTile(SpriteKind.Projectile, assets.tile`red_lever_up`, function (sprite, location) {
+    sprites.destroy(interactbox)
 })
